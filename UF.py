12ba3a1a88fd504b75370052c7f95f52026b4f38
Valentin <val3k@yandex.ru>
2020-02-20 21:37:56 +0300

    weighted UF

--- UF.py
+++ UF.py
@@ -2,9 +2,11 @@ class UnionFind:
     def __init__(self, N):
         self.N = N
         self.obj_set = []
+        self.sz = []
         for i in range(N):
             self.obj_set.append(i)
-    
+            self.sz.append(1)
+
     def _get_root(self, p):
         r = self.obj_set[p]
         while r != self.obj_set[r]:
@@ -17,13 +19,19 @@ class UnionFind:
         else:
             return False
 
-    def union(self, p, q):
-        if self.find(p,q):
+    def union(self, q, p):
+        if self.find(p, q):
             return
         else:
             new = self._get_root(q)
             old = self._get_root(p)
-            self.obj_set[old] = new
+            if self.sz[new] >= self.sz[old]:
+                self.obj_set[old] = new
+                self.sz[new] += self.sz[old]
+            else:
+                self.obj_set[new] = old
+                self.sz[old] += self.sz[new]
+
 
 DC = UnionFind(10)
 DC.union(4, 3)
