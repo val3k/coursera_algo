d2314913cb6586438a38910595242b2d53b2b46a
Valentin <val3k@yandex.ru>
2020-06-06 10:18:11 +0300

    tests for merge sort

---
+++ w3/tests/test_sort.py
@@ -0,0 +1,30 @@
+import numpy as np
+from Sorting import merge, mergesort
+
+
+def test_sort():
+    # setup
+    n = np.random.randint(3, 10, size=1)[0]
+    in_ls = np.arange(n)
+    np.random.shuffle(in_ls)
+    # exercise
+    sort_ls = mergesort(in_ls)
+    # verify
+    assert np.array_equal(sort_ls, np.arange(n))
+
+
+def test_trivial():
+    # setup
+    ls = np.array([2, 1])
+    # ex
+    out = mergesort(ls)
+    # verify
+    assert np.array_equal(out, np.array([1, 2]))
+
+
+def test_merge():
+    a1 = [2, 5]
+    a2 = [1, 3, 4]
+    a = merge(a1, a2)
+    assert np.array_equal(a, np.arange(1, 6))
+
