184ede1a04b7811521525594a2030214fc238afd
valentine <valentine@pop-os.localdomain>
2020-09-01 23:41:39 +0300

    update test

--- w3/tests/test_CP.py
+++ w3/tests/test_CP.py
@@ -1,18 +1,21 @@
-from CollinearPoints import BruteCollinearPoints, Point
+from CollinearPoints import BruteCollinearPoints, Point, FastCollinearPoints
 
 
-# def test_BF():
-#     inp = [(10000, 0), (0, 10000), (3000, 7000), (7000, 3000), (20000, 21000), (3000, 4000), (14000, 15000), (6000, 7000)]
-#     points = []
-#     for p in inp:
-#         points.append(Point(p[0], p[1]))
-#     bcp = BruteCollinearPoints(points)
-#     assert bcp.numberOfSegments() == 2
+def test_brute():
+    inp = [(1, 1), (2, 2), (3, 3), (4, 4), (1, 2),
+           (4, 1), (3, 2), (2, 3), (1, 4)]
+    points = []
+    for p in inp:
+        points.append(Point(p[0], p[1]))
+    bcp = BruteCollinearPoints(points)
+    assert bcp.numberOfSegments() == 2
 
 
-def test_slope():
-    p1 = Point(1, 1)
-    p2 = Point(2, 2)
-    p3 = Point(2, 4)
-    #assert p1.slopeTo(p2) == float('inf')
-    assert p1.slopeOrder(p2, p3) == 0
+def test_fast():
+    inp = [(1, 1), (2, 2), (3, 3), (4, 4), (1, 2),
+           (4, 1), (3, 2), (2, 3), (1, 4)]
+    points = []
+    for p in inp:
+        points.append(Point(p[0], p[1]))
+    fcp = FastCollinearPoints(points)
+    assert fcp.numberOfSegments() == 2
