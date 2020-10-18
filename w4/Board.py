154c5f0eeda6fa6c5ed5050347bded3d86e195c3
valentine <valentine@pop-os.localdomain>
2020-10-18 19:22:55 +0300

    all public methods except neighbors

--- w4/Board.py
+++ w4/Board.py
@@ -4,7 +4,58 @@ import random
 class Board():
     def __init__(self, n_tiles=3):
         self.n = n_tiles
-        self.tiles = random.randint(1, self.n**2-1)
+        self.tiles = random.sample(range(self.n**2), self.n**2)
+        self.dim = n_tiles ** 2
+        self.goal = [i for i in range(1, self.dim)] + [0]
 
     def __str__(self):
-        print(self.tiles)
+        s = ''
+        for i, j in enumerate(self.tiles):
+            s += str(j)
+            if (i+1) % 3 == 0:
+                s += '\n'
+            else:
+                s += ' '
+        return s
+
+    def hamming(self):
+        h = 0
+        for i in range(self.dim):
+            if self.tiles[i] != self.goal[i]:
+                h += 1
+        return h
+
+    def manhattan(self):
+        m = 0
+        for i in range(self.dim):
+            if self.tiles[i] != self.goal[i]:
+                m += self.get_steps(i, self.tiles[i])
+        return m
+
+    def get_steps(self, position, element):
+        row = position // self.n
+        col = position % self.n
+        if element != 0:
+            trow = (element - 1) // self.n
+            tcol = (element - 1) % self.n
+        else:
+            trow = self.n - 1
+            tcol = self.n - 1
+        return abs(tcol - col) + abs(trow - row)
+
+    def isGoal(self):
+        return self.hamming == 0
+
+    def equals(self, board):
+        assert self.n == board.n
+        if self.tiles == board.tiles:
+            return True
+        else:
+            return False
+
+
+
+
+
+
+            
--- w4/tests/test_board.py
+++ w4/tests/test_board.py
@@ -3,5 +3,45 @@ from Board import Board
 
 def test_str():
     b = Board()
+    b.tiles = [0, 2, 3, 1, 4, 5, 6, 7, 8]
     str_b = str(b)
-    assert str_b == '0 2 3/n 1 4 5/n 6 7 8'
+    print(str_b)
+    assert str_b == '0 2 3\n1 4 5\n6 7 8\n'
+
+
+def test_hamming():
+    b = Board()
+    b.tiles = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+    assert b.hamming() == 0
+    b.tiles = [0, 2, 3, 1, 4, 5, 6, 7, 8]
+    assert b.hamming() == 7
+
+
+def test_steps():
+    b = Board()
+    assert b.get_steps(0, 8) == 3
+    assert b.get_steps(5, 4) == 2
+    assert b.get_steps(6, 3) == 4
+    assert b.get_steps(4, 0) == 2
+
+
+def test_manhattan():
+    b = Board()
+    b.tiles = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+    assert b.manhattan() == 0
+    b.tiles = [1, 2, 3, 0, 5, 6, 7, 8, 4]
+    assert b.manhattan() == 6
+
+
+def test_equals():
+    a = Board()
+    a.tiles = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+    b = Board()
+    b.tiles = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+    c = Board()
+    c.tiles = [0, 2, 3, 4, 5, 6, 7, 8, 1]
+    assert a.equals(b)
+    assert not b.equals(c)
+
+
+
